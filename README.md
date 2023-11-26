2e22f58bade851637165a68d7dab293de9b396d8
Iraz Irfan <irazirfan@gmail.com>
2023-11-26 21:15:29 +0600

    adding colors for output & instructions in README

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # haskell_sort_algo
+```powershell
+# Use this command to install Haskell on a Windows machine
+Set-ExecutionPolicy Bypass -Scope Process -Force;[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; try { Invoke-Command -ScriptBlock ([ScriptBlock]::Create((Invoke-WebRequest https://www.haskell.org/ghcup/sh/bootstrap-haskell.ps1 -UseBasicParsing))) -ArgumentList $true } catch { Write-Error $_ }
+
+# Additionally, you need to run these commands to install ansi-terminal for adding color to the output by using ANSI escape codes
+cabal update
+cabal install ansi-terminal
--- sort_algo.hs
+++ sort_algo.hs
@@ -1,4 +1,6 @@
+import System.Console.ANSI
 import Data.List (sort)
+
 
 -- Sorting algorithms
 
@@ -40,9 +42,17 @@ quickSort (x:xs) = quickSort (filter (< x) xs) ++ [x] ++ quickSort (filter (>= x
 
 -- User interface functions
 
+setColor :: Color -> IO ()
+setColor color = setSGR [SetColor Foreground Vivid color]
+
+resetColor :: IO ()
+resetColor = setSGR [Reset]
+
 displayMenu :: IO ()
 displayMenu = do
+  setColor Cyan
   putStrLn "\nSorting Algorithm Simulation"
+  resetColor
   putStrLn "1. Selection Sort"
   putStrLn "2. Bubble Sort"
   putStrLn "3. Insertion Sort"
@@ -51,13 +61,17 @@ displayMenu = do
 
 getAlgorithmChoice :: IO Int
 getAlgorithmChoice = do
+  setColor Yellow
   putStr "Choose a sorting algorithm (1-5): "
+  resetColor
   choice <- getLine
   return (read choice :: Int)
 
 getOrderChoice :: IO Int
 getOrderChoice = do
+  setColor Yellow
   putStrLn "Choose sorting order:"
+  resetColor
   putStrLn "1. Ascending"
   putStrLn "2. Descending"
   putStr "Enter your choice (1 or 2): "
@@ -69,12 +83,16 @@ getDefaultArray = [5, 2, 8, 1, 3]
 
 getCustomArray :: IO [Int]
 getCustomArray = do
+  setColor Yellow
   putStrLn "Enter custom array elements separated by spaces (maximum 5 elements):"
+  resetColor
   input <- getLine
   let customArray = take 5 $ map read (words input) :: [Int]
   if length customArray /= length (words input)
     then do
+      setColor Red
       putStrLn "Error: Too many elements provided. Only the first 5 elements will be considered."
+      resetColor
     else return ()
   return customArray
 
@@ -82,28 +100,17 @@ validateArrayRange :: [Int] -> Bool
 validateArrayRange [] = False
 validateArrayRange array = all (\x -> x >= 0 && x <= 100) array
 
-main :: IO ()
-main = do
-  displayMenu
-  algorithmChoice <- getAlgorithmChoice
-
-  case algorithmChoice of
-    1 -> simulateSort (selectionSort :: [Int] -> [Int])
-    2 -> simulateSort (bubbleSort :: [Int] -> [Int])
-    3 -> simulateSort (insertionSort :: [Int] -> [Int])
-    4 -> simulateSort (mergeSort :: [Int] -> [Int])
-    5 -> simulateSort (quickSort :: [Int] -> [Int])
-    _ -> putStrLn "Invalid input. Please enter a number between 1 and 5."
-
 simulateSort :: ([Int] -> [Int]) -> IO ()
 simulateSort sortFunction = do
   orderChoice <- getOrderChoice
   let array = getDefaultArray
   let sortedArray = if orderChoice == 1 then sortFunction array else reverse (sortFunction array)
 
+  setColor Magenta
   putStrLn $ "\nDefault Array: " ++ show array
   putStrLn $ "Sorting in " ++ (if orderChoice == 1 then "Ascending" else "Descending") ++ " order..."
   putStrLn $ "Sorted Array: " ++ show sortedArray
+  resetColor
 
   putStrLn "Do you want to input a custom array? (y/n)"
   customArrayOption <- getLine
@@ -115,10 +122,15 @@ simulateSort sortFunction = do
 
       if validateArrayRange customArray
         then do
+          setColor Magenta
           putStrLn $ "\nCustom Array: " ++ show customArray
           putStrLn $ "Sorting in " ++ (if orderChoice == 1 then "Ascending" else "Descending") ++ " order..."
           putStrLn $ "Sorted Custom Array: " ++ show customSortedArray
-        else putStrLn "Invalid input. Please provide a valid array in the specified range: arrays of size 5 within the range of 0 to 100."
+          resetColor
+        else do
+          setColor Red
+          putStrLn "Invalid input. Please provide a valid array in the specified range: arrays of size 5 within the range of 0 to 100."
+          resetColor
 
     _ -> return ()
 
@@ -127,4 +139,25 @@ simulateSort sortFunction = do
 
   if restartOption == "y"
     then main
-    else putStrLn "Goodbye!"
+    else do
+      setColor Green
+      putStrLn "Exiting program, Goodbye!"
+      resetColor
+
+main :: IO ()
+main = do
+  setColor Green
+  displayMenu
+  resetColor
+  algorithmChoice <- getAlgorithmChoice
+
+  case algorithmChoice of
+    1 -> simulateSort (selectionSort :: [Int] -> [Int])
+    2 -> simulateSort (bubbleSort :: [Int] -> [Int])
+    3 -> simulateSort (insertionSort :: [Int] -> [Int])
+    4 -> simulateSort (mergeSort :: [Int] -> [Int])
+    5 -> simulateSort (quickSort :: [Int] -> [Int])
+    _ -> do
+      setColor Red
+      putStrLn "Invalid input. Please enter a number between 1 and 5."
+      resetColor
